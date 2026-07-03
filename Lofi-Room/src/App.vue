e9cd49c26998b5b527c4c95a5ecc3635de798ca5
takkun2355 <92196121+takkun2355@users.noreply.github.com>
2026-07-03 22:53:52 +0900

    Vue Project に 変更1
    とりあえずいったんこれでおｋ！！！

--- Lofi-Room/src/App.vue
+++ Lofi-Room/src/App.vue
@@ -1,11 +1,47 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <main>
+
+    <h1>📚 Lofi Room</h1>
+
+    <p>Virtual Library</p>
+
+    <SeatCard />
+
+  </main>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+
+import SeatCard from "./components/SeatCard.vue"
+
+</script>
+
+<style scoped>
+
+main {
+
+  min-height: 100vh;
+
+  background: #111827;
+
+  color: white;
+
+  display: flex;
+
+  flex-direction: column;
+
+  align-items: center;
+
+  justify-content: center;
+
+  gap: 12px;
+
+}
+
+h1 {
+
+  font-size: 48px;
+
+}
+
+</style>
---
+++ Lofi-Room/src/components/SeatCard.vue
@@ -0,0 +1,33 @@
+<template>
+
+<div class="seat">
+
+<h3>席01</h3>
+
+<p>Takkun2355</p>
+
+<p>Test</p>
+
+</div>
+
+</template>
+
+<style scoped>
+
+.seat{
+
+width:220px;
+
+height:140px;
+
+background:#1f2937;
+
+border-radius:16px;
+
+padding:16px;
+
+box-shadow:0 0 20px rgba(0,0,0,.3);
+
+}
+
+</style>
